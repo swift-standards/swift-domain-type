dbeb4ba22a72ebb65b75876f79b90e829169ca9e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:03:16 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -2,21 +2,6 @@
 
 import PackageDescription
 
-extension String {
-    static let domain: Self = "Domain Standard"
-}
-
-extension Target.Dependency {
-    static var domain: Self { .target(name: .domain) }
-}
-
-extension Target.Dependency {
-    static var rfc1035: Self { .product(name: "RFC 1035", package: "swift-rfc-1035") }
-    static var rfc1123: Self { .product(name: "RFC 1123", package: "swift-rfc-1123") }
-    static var rfc5321: Self { .product(name: "RFC 5321", package: "swift-rfc-5321") }
-    static var rfc5890: Self { .product(name: "RFC 5890", package: "swift-rfc-5890") }
-}
-
 let package = Package(
     name: "swift-domain-standard",
     platforms: [
@@ -38,23 +23,21 @@ let package = Package(
         .target(
             name: "Domain Standard",
             dependencies: [
-                .rfc1035,
-                .rfc1123,
-                .rfc5321,
-                .rfc5890,
+                .product(name: "RFC 1035", package: "swift-rfc-1035"),
+                .product(name: "RFC 1123", package: "swift-rfc-1123"),
+                .product(name: "RFC 5321", package: "swift-rfc-5321"),
+                .product(name: "RFC 5890", package: "swift-rfc-5890"),
             ]
         ),
         .testTarget(
             name: "Domain Standard Tests",
             dependencies: [
-                "Domain Standard"
+                .target(name: "Domain Standard")
             ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-extension String { var tests: Self { self + " Tests" } }
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
     let ecosystem: [SwiftSetting] = [
